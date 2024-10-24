741506739b2dd7b60ccc358838133d8709c6060e
Lean <113753207@nccu.edu.tw>
2024-10-24 17:49:41 +0800

    Refactor app to use environment variables and improve MongoDB connection: - Use process.env.MONGODB_URI for MongoDB URI. - Load environment variables with dotenv. - Replace express.json() with bodyParser.json(). - Validate MONGODB_URI and SECRET_KEY. - Update route to '/api/users'.

--- src/app.js
+++ src/app.js
@@ -1,32 +1,44 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const authRoutes = require("./routes/UserRoutes");
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const UserRoutes = require('./routes/UserRoutes');
 
 const app = express();
-// 讀 json 格式
-app.use(express.json());
+app.use(bodyParser.json());
 
-// connect to mongodb
 
-mongoose
-  .connect("mongodb://localhost:27017/authDemo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true, // 使用 createIndex()，避免 ensureIndex() 的警告
-  })
-  // 設定連線成功與失敗的 response
-  .then(() => {
-    console.log("connected to mongodb");
-  })
-  .catch((err) => {
-    console.log("MongoDB connection error:", err);
-  });
+// Test .env
+console.log('MONGODB_URI:', process.env.MONGODB_URI);
+console.log('SECRET_KEY:', process.env.SECRET_KEY);
 
-// 設置路由，當路徑以 '/auth' 開頭時，交由 authRoutes 來處理
-app.use("/auth", authRoutes);
 
-// 啟動伺服器
-app.listen(3000, () => {
-  // 伺服器成功啟動後，顯示提示訊息
-  console.log("Server running on http://localhost:3000");
+// Get environment variables
+const uri = process.env.MONGODB_URI;
+const secretKey = process.env.SECRET_KEY;
+
+
+// Check URI and secretKey
+if (!uri) {
+  console.error('error: undefined MONGODB_URI');
+  process.exit(1);
+}
+if (!secretKey) {
+  console.error('error: undefined SECRET_KEY');
+  process.exit(1);
+}
+
+// connect to MongoDB
+mongoose.connect(uri)
+  .then(() => console.log('success: connected to MongoDB'))
+  .catch(err => console.error('fail connected to MongoDB', err));
+
+// 使用 UserRoutes
+app.use('/api/users', UserRoutes);
+
+// start server in env.PORT or 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`server is running ${PORT}`);
 });
