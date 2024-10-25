e2b6751f3c397d11a6e3a3b3ea6c1efad0020dd8
shiloh <shilohlee98@gmail.com>
2024-10-25 13:19:09 +0800

    Modify path due to middleware rename

--- src/routes/apiRoutes.js
+++ src/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userRoutes = require("./userRoutes");
-const auth = require("../middlewares/AuthenticationMiddleware");
+const auth = require("../middlewares/authenticationMiddleware");
 
 router.use("/user", userRoutes);
 // router.use("/userRelationship", userRelationshipRoutes);
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const auth = require('../middlewares/AuthenticationMiddleware');
+const auth = require('../middlewares/authenticationMiddleware');
 
 // register route
 router.post('/register', userController.register);
