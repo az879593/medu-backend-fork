2d752d08a6e9b27537c329806d447129ee5042cf
shiloh lee <shilohlee98@gmail.com>
2024-11-11 20:40:05 +0800

    Refactor userController and Add userService - moved the business logic from the userController to the newly created userService - use APIError to pass the error from service to controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,62 +1,19 @@
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
-const User = require('../models/User');
+const userService = require('../services/userService');
 
 // register
 exports.register = async (req, res) => {
     try {
         const { username, password, nickname, birthDate, gender } = req.body;
+        const userData = { username, password, nickname, birthDate, gender };
 
-        // 檢查必填欄位是否為空
-        if (!username?.trim() || !password?.trim() || !nickname?.trim() || !birthDate || !gender?.trim()) {
-            return res
-                .status(400)
-                .json({ message: '請提供完整的註冊資訊' });
-        }
-
-        // 檢查用戶是否已存在
-        let user = await User.findOne({ 'username': username });
-        if (user) {
-            return res.status(400).json({ message: 'User is existed' });
-        }
-
-        // 創建新用戶
-        user = new User({
-            profile: {
-                nickname,
-                birthDate,
-                gender,
-            },
-            username,
-            password,
-        });
-        await user.save();
+        await userService.register(userData);
 
         res.status(201).json({ message: 'register success' });
     } catch (error) {
-        if (error.name === 'ValidationError') {
-            let message = '';
-            for (const field in error.errors) {
-                if (error.errors[field].path === 'profile.birthDate') {
-                    message += '請提供有效的生日';
-                } else if (error.errors[field].path === 'profile.gender') {
-                    message += '請提供有效的性別';
-                } else if (error.errors[field].path === 'username') {
-                    message += '請提供有效的用戶名';
-                } else if (error.errors[field].path === 'profile.nickname') {
-                    message += '請提供有效的暱稱';
-                } else if (error.errors[field].path === 'password') {
-                    message += '密碼長度應至少為6個字符';
-                } else {
-                    message += error.errors[field].message + ' ';
-                }
-            }
-            return res.status(400).json({ message: message.trim() });
+        if(error.name === 'APIError'){
+            return res.status(error.statusCode).json({ message: error.message.trim() });
         }
-
-
-        console.error(error); // 方便調試
-        res.status(500).json({ message: 'server error' });
+        return res.status(500).json({ message: message.trim() });
     }
 };
 
@@ -65,31 +22,14 @@ exports.login = async (req, res) => {
     try {
         const { username, password } = req.body;
 
-        // 檢查必填欄位是否為空
-        if (!username?.trim() || !password?.trim()) {
-            return res.status(400).json({ message: '請提供用戶名和密碼' });
-        }
-
-        // 檢查用戶是否存在
-        const user = await User.findOne({ 'username': username });
-        if (!user) {
-            return res.status(400).json({ message: '用戶不存在' });
-        }
-
-        // 驗證密碼
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ message: '密碼錯誤' });
-        }
-
-        // 生成 JWT
-        const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY, {
-            expiresIn: '1h', // 1 hour expires
-        });
+        const token = await userService.login(username, password);
 
         res.json({ token });
     } catch (error) {
-        console.error(error); // 方便調試
+        if(error.name === "APIError") {
+            return res.status(error.statusCode).json({ message: error.message.trim() });
+        }
+        // console.error(error); // 方便調試
         res.status(500).json({ message: '伺服器錯誤' });
     }
 };
