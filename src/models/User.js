39eeb5056b0ab1b24ca9547861c5fb8412c93c08
shiloh lee <shilohlee98@gmail.com>
2024-11-12 16:11:24 +0800

    Modify user nickname to non unique

--- src/models/User.js
+++ src/models/User.js
@@ -6,7 +6,7 @@ const UserSchema = new mongoose.Schema({
         nickname: {
             type: String,
             // required: true,
-            unique: true,
+            // unique: true,
             // trim: true,
             minlength: 3,
             maxlength: 30,
