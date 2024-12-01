7e1d99012555107cd4f18482fbf5397192aca7c6
shiloh lee <shilohlee98@gmail.com>
2024-12-01 17:50:53 +0800

    Finish the main function of messageController.js

---
+++ src/controllers/messageController.js
@@ -0,0 +1,41 @@
+const messageService = require('../services/messageService');
+const APIError = require('../errors/APIError');
+
+exports.sendMessage = async (req, res) => {
+    try {
+        const savedMessage = await messageService.sendMessage(req.user.userId, req.body.targetUserId, req.body.message);
+        res.json({ message: savedMessage });
+    } catch(error) {
+        if(error instanceof APIError){
+            res.status(error.statusCode).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+exports.getAllMessageHistory = async (req, res) => {
+    try {
+        const messageHistory = await messageService.getAllMessageHistoryByUserId(req.user.userId, req.body.targetUserId);
+        res.status(200).json({ messageHistory: messageHistory || [] });
+    } catch(error) {
+        if(error instanceof APIError){
+            res.status(error.statusCode).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
+
+exports.getLatestMessage = async (req, res) => {
+    try {
+        const latestMessage = await messageService.getLatestMessage(req.user.userId, req.body.targetUserId);
+        res.status(200).json({ latestMessage: latestMessage });
+    } catch(error) {
+        if(error instanceof APIError){
+            res.status(error.statusCode).json({ message: error.message });
+        } else {
+            res.status(500).json({ message: error.message });
+        }
+    }
+}
