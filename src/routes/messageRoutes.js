4c39f5e091ada7119b3d522940afad439dba6757
shiloh lee <shilohlee98@gmail.com>
2024-12-03 13:44:13 +0800

    Close the route to message/send - only websocket server can use this function to send message

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const messageController = require('../controllers/messageController');
 const auth = require('../middlewares/authenticationMiddleware');
 
-router.post('/send', auth, messageController.sendMessage);
+// router.post('/send', auth, messageController.sendMessage);
 router.get('/messagehistory', auth, messageController.getAllMessageHistory);
 router.get('/latestmessage', auth, messageController.getLatestMessage);
 
